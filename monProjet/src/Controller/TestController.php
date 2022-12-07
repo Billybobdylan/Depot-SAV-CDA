db9e83e61b976f9fb0f9a8693303d33eb8122ec4
BillybobDylan <creteldylan@gmail.com>
2022-12-07 16:47:00 +0100

    vrai

--- monProjet/src/Controller/TestController.php
+++ monProjet/src/Controller/TestController.php
@@ -2,6 +2,15 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use App\Entity\Souscat;
+use App\Repository\SouscatRepository;
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
+use App\Entity\Detail;
+use App\Repository\DetailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +18,58 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/', name: 'app_')]
-    public function index(): Response
+    public function index(CategorieRepository $repo): Response
     {
+        $categories = $repo->findAll();
         return $this->render('/index.html.twig', [
-            'controller_name' => 'Controller',
+        'categories' => $categories
+    ]);       
+    }
+
+    
+    #[Route('/categorie', name:'categorie')] 
+    public function categorie(CategorieRepository $repo) :Response
+    {
+        $categories = $repo->findAll();
+
+        return $this->render('/categorie.html.twig', [
+            'categories' => $categories
         ]);
-        
+    }
+    
+    #[Route('/categorie/{id}', name: 'app_categorie')]
+    public function souscat($id, CategorieRepository $repo): Response
+    {
+        $cat = $repo->find($id);
+
+        // dd($souscat);
+        return $this->render('/souscat.html.twig', [
+            
+            'cat' => $cat
+        ]);
+    }
+
+    #[Route('/categorie/{id}/{produit}', name: 'app_produit')]
+    public function produit($produit, SouscatRepository $repo): Response
+    {
+        $produit = $repo->find($produit);
+
+        // dd($produit);
+        return $this->render('/produit.html.twig', [
+            
+            'produit' => $produit
+        ]);
+    }
+
+    #[Route('/categorie/{id}/{produit}/{detail}', name: 'app_detail')]
+    public function detail($detail, ProduitRepository $repo): Response
+    {
+        $detail = $repo->find($detail);
+
+         //dd($detail);
+        return $this->render('/detail.html.twig', [
+           
+            'detail' => $detail
+        ]);
     }
 }
